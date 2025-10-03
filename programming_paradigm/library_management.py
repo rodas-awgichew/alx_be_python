5f6cf2175fae43110d175fd3f57d812df9930383
Sadorra <rodiawg@gmail.com>
2025-10-03 14:14:52 +0300

    Implemented Book class.

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,16 @@
+class Book:
+    """Represents a book with a title, author, and checkout status."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
