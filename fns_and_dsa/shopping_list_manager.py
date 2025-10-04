cf8dad605884fbc1acbaf66a9c9234ed695d003e
Sadorra <rodiawg@gmail.com>
2025-10-04 19:13:16 +0300

    fix: requirment fix

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,11 +1,12 @@
 # shopping_list_manager.py
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
+
 
 def main():
     shopping_list = []
